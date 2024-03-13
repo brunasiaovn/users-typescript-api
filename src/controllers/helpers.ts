8f37f86b9f67e5bddf6d530cb0a81a58fea4736e
Bruna <bruna.dev@hotmail.com>
2024-03-13 18:13:29 -0300

    refactor: add HttpStatusCode enum

--- src/controllers/helpers.ts
+++ src/controllers/helpers.ts
@@ -1,23 +1,23 @@
-import { HttpResponse } from "./protocols";
+import { HttpResponse, HttpStatusCode } from "./protocols";
 
 export const ok = <T>(body: unknown): HttpResponse<T> => ({
-  statusCode: 201,
+  statusCode: HttpStatusCode.OK,
   body,
 });
 
 export const created = <T>(body: unknown): HttpResponse<T> => ({
-  statusCode: 201,
+  statusCode: HttpStatusCode.CREATED,
   body,
 });
 
 export const badRequest = (message: string): HttpResponse<string> => {
   return {
-    statusCode: 400,
+    statusCode: HttpStatusCode.BAD_REQUEST,
     body: message,
   };
 };
 
 export const serverError = (): HttpResponse<string> => ({
-  statusCode: 500,
+  statusCode: HttpStatusCode.SERVER_ERROR,
   body: "Something went wrong",
 });
